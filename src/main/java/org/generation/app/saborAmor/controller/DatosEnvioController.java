37c7107d6c054fc28ff5bb535abcc24f59395f41
Jose <jose.mar.123@hotmail.com>
2023-03-06 20:09:27 -0600

    Another attempt at fixing CORS error

--- src/main/java/org/generation/app/saborAmor/controller/DatosEnvioController.java
+++ src/main/java/org/generation/app/saborAmor/controller/DatosEnvioController.java
@@ -5,13 +5,11 @@ import java.util.List;
 import org.generation.app.saborAmor.model.DatosEnvio;
 import org.generation.app.saborAmor.service.IDatosEnvioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api")
+@CrossOrigin
 public class DatosEnvioController {
 
     @Autowired
--- src/main/java/org/generation/app/saborAmor/controller/PedidoProductoController.java
+++ src/main/java/org/generation/app/saborAmor/controller/PedidoProductoController.java
@@ -4,15 +4,13 @@ import org.generation.app.saborAmor.model.Pedido;
 import org.generation.app.saborAmor.model.PedidoProducto;
 import org.generation.app.saborAmor.service.PedidoProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("api")
+@CrossOrigin
 public class PedidoProductoController {
 
     @Autowired
